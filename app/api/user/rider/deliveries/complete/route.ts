8dfbaa20326ce562f232ce566d24247af2601d78
unknown <mlprolygin@gmail.com>
2026-06-21 09:58:43 -0300

    SELLER_HIT as api key

--- app/api/user/rider/deliveries/complete/route.ts
+++ app/api/user/rider/deliveries/complete/route.ts
@@ -85,7 +85,7 @@ export async function POST(request: Request) {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "x-api-key": process.env.INTERNAL_API_KEY!,
+            "x-api-key": process.env.SELLER_HIT!,
           },
         }
       )
